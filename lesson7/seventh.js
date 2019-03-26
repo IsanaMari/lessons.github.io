fe895c5f105d1b82aaface67414deb2b79348f8a
Janna-pavlenko <jannapavl@gmail.com>
2019-03-26 17:57:13 +0300

    changes

--- lesson7/seventh.js
+++ lesson7/seventh.js
@@ -1,25 +1,25 @@
 function imageAppear() {
-    var first = document.getElementById('first');
-    var p = first.appendChild(document.createElement('p'));
-    p.innerText = 'Click me';
-    p.addEventListener('click', function (event) {
-        var img = first.appendChild(document.createElement('img'));
-        img.setAttribute('src', 'https://picsum.photos/200/200/?random');
-        img.style = `
+  var first = document.getElementById('first');
+  var p = first.appendChild(document.createElement('p'));
+  p.innerText = 'Click me';
+  p.addEventListener('click', function (event) {
+    var img = first.appendChild(document.createElement('img'));
+    img.setAttribute('src', 'https://picsum.photos/200/200/?random');
+    img.style = `
             height: 100px;
             width: 100px;
         `;
-        img.addEventListener('mouseover', function (event) {
-            img.style = `
+    img.addEventListener('mouseover', function (event) {
+      img.style = `
             transition: all 1s;
             height: 200px;
             width: 200px;
         `
-        });
-        img.addEventListener('click', function (event) {
-            img.remove();
-        })
     });
+    img.addEventListener('click', function (event) {
+      img.remove();
+    })
+  });
 
 }
 
@@ -40,9 +40,12 @@ var second = document.getElementById('second');
 // }
 var a = {};
 ["first", "second", "third", "fourth"].forEach(
-    function (tag, index, arr) {
-        console.log(a[tag] = collection[index])
-        second.appendChild(document.createElement(collection[index]))
-    }
-)
-console.log(a)
+  function (tag, index, arr) {
+    // console.log(a[tag] = collection[index]);
+    // var b = second.appendChild(document.createElement(collection[index]))
+    // b.appendChild(document.createElement(collection[index + 1]))
+    // second.appendChild(document.createElement(collection[index]))
+    console.log(second.appendChild(document.createElement(collection[index + 1]))).appendChild(document.createElement(collection[index]))
+  }
+);
+console.log(a);
---
+++ lesson8/eight.js
@@ -0,0 +1,7 @@
+(function () {
+  var first = document.getElementById('first');
+  var today = new Date();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = time;
+  // console.log(dateTime)
+})();
