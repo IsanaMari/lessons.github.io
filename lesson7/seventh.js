67f17dd29e61ac429667381ba8a2dd0bb0fcfd30
Janna-pavlenko <jannapavl@gmail.com>
2019-03-28 17:48:32 +0300

    changes

--- lesson7/seventh.js
+++ lesson7/seventh.js
@@ -1,25 +1,25 @@
 function imageAppear() {
-    var first = document.getElementById('first');
-    var p = first.appendChild(document.createElement('p'));
-    p.innerText = 'Click me';
-    p.addEventListener('click', function (event) {
-        var img = first.appendChild(document.createElement('img'));
-        img.setAttribute('src', 'https://picsum.photos/200/200/?random');
-        img.style = `
+  var first = document.getElementById('first');
+  var p = first.appendChild(document.createElement('p'));
+  p.innerText = 'Click me';
+  p.addEventListener('click', function (event) {
+    var img = first.appendChild(document.createElement('img'));
+    img.setAttribute('src', 'https://picsum.photos/200/200/?random');
+    img.style = `
             height: 100px;
             width: 100px;
         `;
-        img.addEventListener('mouseover', function (event) {
-            img.style = `
+    img.addEventListener('mouseover', function (event) {
+      img.style = `
             transition: all 1s;
             height: 200px;
             width: 200px;
         `
-        });
-        img.addEventListener('click', function (event) {
-            img.remove();
-        })
     });
+    img.addEventListener('click', function (event) {
+      img.remove();
+    })
+  });
 
 }
 
@@ -27,50 +27,50 @@ imageAppear();
 
 /*Second*/
 var collection = [
-    {
-        name: 'div',
-        attrs:{
-            title: 'container',
-            style: `
+  {
+    name: 'div',
+    attrs: {
+      title: 'container',
+      style: `
                 background-color: #ff0066;
                 border: dotted 2px #ff0000;
                 border-radius: 50%;
             `
-        }
-    },
-    {
-        name: 'p',
-        attrs:{
-            title: 'container',
-            style: `
-                background-color: #ff0066;
-                border: dotted 2px #ff0000;
-                border-radius: 50%;
-            `
-        }
-    },
-    {
-        name: 'span',
-        attrs:{
-            title: 'container',
-            style: `
-                background-color: #ff0066;
-                border: dotted 2px #ff0000;
-                border-radius: 50%;
-            `
-        }
-    },
-    {
-        name: 'a',
-        attrs:{
-            title: 'container',
-            style: `
-                background-color: #ff0066;
-                border: dotted 2px #ff0000;
-                border-radius: 50%;
-            `
-        }
     }
+  },
+  {
+    name: 'p',
+    attrs: {
+      title: 'container',
+      style: `
+                background-color: #ff0066;
+                border: dotted 2px #ff0000;
+                border-radius: 50%;
+            `
+    }
+  },
+  {
+    name: 'span',
+    attrs: {
+      title: 'container',
+      style: `
+                background-color: #ff0066;
+                border: dotted 2px #ff0000;
+                border-radius: 50%;
+            `
+    }
+  },
+  {
+    name: 'a',
+    attrs: {
+      title: 'container',
+      style: `
+                background-color: #ff0066;
+                border: dotted 2px #ff0000;
+                border-radius: 50%;
+            `
+    }
+  }
 ];
 var second = document.getElementById('second');
 //
@@ -85,32 +85,12 @@ var second = document.getElementById('second');
 // }
 var el = [];
 ["first", "second", "third", "fourth"].forEach(
-    function insertElement(tag, index, arr) {
-        var el = index.appendChild(document.createElement(collection.name));
-        console.log(el)
-        el.num = tag;
-        return el
+  function (tag, index, arr) {
+    let j = document.getElementsByTagName('body')[0];
+    for (let ind = 0; ind < index; ind++) {
+      j = j.firstChild
     }
-);
-el [0] = insertElement(0, document.body)
-for (var x = 1; x < 5; x++) {
-    el [x] = insertElement(x, el [x - 1])
-}
-// console.log(a);
-// var elemData = {
-//     name: "div"
-// }
-//
-// function insertElement(elemNum, parentElem) {
-//     var elem = parentElem.appendChild(
-//         document.createElement(elemData.name)
-//     )
-//     elem.num = elemNum
-//     return elem
-// }
-//
-// var elems = []
-// elems [0] = insertElement(0, document.body)
-// for (var x = 1; x < 5; x++) {
-//     elems [x] = insertElement(x, elems [x - 1])
-// }
+    j.prepend(document.createElement(collection[index].name));
+    j = document.getElementsByTagName('body')[0];
+  }
+)
--- lesson8/eight.js
+++ lesson8/eight.js
@@ -1,7 +1,28 @@
-(function () {
-  var first = document.getElementById('first');
-  var today = new Date();
-  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  var dateTime = time;
-  // console.log(dateTime)
-})();
+// var first = document.getElementById('first');
+// var today;
+//
+// function startFunction() {
+//   var a = first.appendChild(document.createElement('p'));
+//   a.innerHTML = today = new Date().toLocaleTimeString();
+// }
+// var i = setInterval(startFunction, 1000);
+// setTimeout(function () {
+//   clearInterval(i);
+// }, 100000);
+
+/*second*/
+
+var typeMessage = (function (velocity) {
+  let container = document.getElementById("demo") ?
+    document.getElementById("demo") :
+    document.body.appendChild(
+      document.createElement("h3")
+    );
+  container.style = `color: magenta;`;
+  var index = 0;
+  return function (message) {
+    console.log(`function[${velocity}]`)
+  }
+})(1);
+
+typeMessage(`Welcome to the hell`);
