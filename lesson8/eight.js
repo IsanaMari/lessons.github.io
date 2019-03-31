a1dfb256d679ed7be955a241f324e7ad9078205a
IsanaMari <jannapavl@gmail.com>
2019-03-31 22:25:27 +0300

    changes

--- lesson8/eight.js
+++ lesson8/eight.js
@@ -1,28 +1,53 @@
-// var first = document.getElementById('first');
-// var today;
-//
-// function startFunction() {
-//   var a = first.appendChild(document.createElement('p'));
-//   a.innerHTML = today = new Date().toLocaleTimeString();
-// }
-// var i = setInterval(startFunction, 1000);
-// setTimeout(function () {
-//   clearInterval(i);
-// }, 100000);
+var first = document.getElementById('first');
+var today;
+
+function startFunction() {
+  var a = first.appendChild(document.createElement('p'));
+  a.innerHTML = today = new Date().toLocaleTimeString();
+}
+var i = setInterval(startFunction, 1000);
+setTimeout(function () {
+  clearInterval(i);
+}, 100000);
 
 /*second*/
 
 var typeMessage = (function (velocity) {
-  let container = document.getElementById("demo") ?
-    document.getElementById("demo") :
-    document.body.appendChild(
-      document.createElement("h3")
-    );
-  container.style = `color: magenta;`;
-  var index = 0;
-  return function (message) {
-    console.log(`function[${velocity}]`)
-  }
+    let container = document.getElementById("second") ?
+        document.getElementById("second") :
+        document.body.appendChild(
+            document.createElement("h3")
+        );
+    container.style = `color: magenta;`;
+    var index = 0;
+    return function (message) {
+        var num = index;
+        setTimeout(
+            () => container.textContent += message [num],
+            1000 * velocity * num
+        );
+        index++ < message.length - 1 ? arguments.callee(message) : null
+    }
 })(1);
 
 typeMessage(`Welcome to the hell`);
+
+/*third*/
+
+function User(name) {
+    this.name = name;
+    this.id = this.counter()
+}
+User.prototype.counter = (function () {
+        var counter = 0;
+        return function () {
+            return this.id ? this.id : counter++
+        }
+    })();
+
+var users = [
+    new User("Семен"),
+    new User("Антон"),
+    new User("Демьян"),
+    new User("Василий")
+]
