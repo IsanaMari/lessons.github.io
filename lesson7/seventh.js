bad1acea93b96b89b758e6bb36b825801fe596eb
Janna-pavlenko <jannapavl@gmail.com>
2019-04-01 15:54:57 +0300

    changes

--- lesson7/seventh.js
+++ lesson7/seventh.js
@@ -1,65 +1,32 @@
 function imageAppear() {
-    var first = document.getElementById('first');
-    var p = first.appendChild(document.createElement('p'));
-    p.innerText = 'Click me';
-    p.addEventListener('click', function (event) {
-        var img = first.appendChild(document.createElement('img'));
-        img.setAttribute('src', 'https://picsum.photos/200/200/?random');
-        img.style = `
+  var first = document.getElementById('first');
+  var p = first.appendChild(document.createElement('p'));
+  p.innerText = 'Click me';
+  p.addEventListener('click', function (event) {
+    var img = first.appendChild(document.createElement('img'));
+    img.setAttribute('src', 'https://picsum.photos/200/200/?random');
+    img.style = `
             height: 100px;
             width: 100px;
         `;
-        img.addEventListener('mouseover', function (event) {
-            img.style = `
+    img.addEventListener('mouseover', function (event) {
+      img.style = `
             transition: all 1s;
             height: 200px;
             width: 200px;
         `
-        });
-        img.addEventListener('click', function (event) {
-            img.remove();
-        })
     });
+    img.addEventListener('click', function (event) {
+      img.remove();
+    })
+  });
 
 }
 
 imageAppear();
 
 /*Second*/
-// var collection = [];
-//
-// function over(event) {
-//     event.target.style.backgroundColor = '#ffff0050'
-// }
-// function out(event) {
-//     event.target.style.backgroundColor = '#ff00ff50'
-// }
-// function clickHandler(event) {
-//     event.target.remove()
-// }
-// var second = document.getElementById('second');
-// ["first", "second", "third", "fourth"].forEach(
-//     function (tag, index, arr) {
-//
-//         var x = (index ? collection[index - 1] : second).appendChild(
-//             document.createElement('div')
-//         );
-//         collection.push(x);
-//         x.style = `
-//       width: ${400 - index * 50}px;
-//       height: ${400 - index * 50}px;
-//       background-color: #ff00ff50;
-//       border: dotted 1px yellow;
-//     `;
-//         x.title = tag;
-//         x.onmouseover = over;
-//         x.onmouseout = out;
-//         x.onclick = clickHandler
-//     }
-// );
-
-/*third*/
-var collection = []
+var collection = [];
 
 function over(event) {
     event.target.style.backgroundColor = '#ffff0050'
@@ -68,10 +35,10 @@ function out(event) {
     event.target.style.backgroundColor = '#ff00ff50'
 }
 function clickHandler(event) {
-    console.log()
+    event.target.remove()
 }
-
-[1, 2, 3, 4, 5, 6, 7].forEach(
+var second = document.getElementById('second');
+["first", "second", "third", "fourth"].forEach(
     function (tag, index, arr) {
 
         var x = (index ? collection[index - 1] : second).appendChild(
@@ -89,4 +56,37 @@ function clickHandler(event) {
         x.onmouseout = out;
         x.onclick = clickHandler
     }
-)
+);
+
+/*third*/
+var elementsCollection = [];
+
+function mouseOver(event) {
+  event.target.style.backgroundColor = '#ffff0050'
+}
+function mouseOut(event) {
+  event.target.style.backgroundColor = '#ff00ff50'
+}
+function clickHandlerEvent(event) {
+  // event.target.innerHTML = '';
+}
+var third = document.getElementById('third');
+[1, 2, 3, 4, 5, 6, 7].forEach(
+  function (tag, index, arr) {
+
+    var x = (index ? elementsCollection[index - 1] : third).appendChild(
+      document.createElement('div')
+    );
+    elementsCollection.push(x);
+    x.style = `
+      width: ${400 - index * 50}px;
+      height: ${400 - index * 50}px;
+      background-color: #ff00ff50;
+      border: dotted 1px yellow;
+    `;
+    x.title = tag;
+    x.onmouseover = mouseOver;
+    x.onmouseout = mouseOut;
+    x.onclick = clickHandlerEvent
+  }
+);
---
+++ lesson9/ninth.js
@@ -0,0 +1,69 @@
+/*First*/
+function typeMessage(message, velocity) {
+  var container = document.getElementById("demo") ?
+    document.getElementById("demo") :
+    document.body.appendChild(
+      document.createElement("h3")
+    );
+  container.style = `color: magenta`;
+  message.split('').forEach(
+    (item, index) => setTimeout(
+      () => container.textContent += item,
+      1000 * index * velocity
+    )
+  )
+}
+
+typeMessage(`Welcome to the hell`, 1);
+
+/*Second*/
+var users = (function (list) {
+  var users = [];
+  for (var user of list)
+    users.push({
+      name: user,
+      present: false
+    })
+
+  return {
+    setUserPresent (userName, present) {
+      let user = users.find(
+        item => item.name === userName
+      );
+      user ? user.present = present : null
+    },
+    showPresent () {
+      console.log(
+        users.filter(
+          item => item.present
+        )
+      )
+
+    },
+    showAbsent () {
+      console.log(
+        users.filter(
+          item => !item.present
+        )
+      )
+    }
+  }
+})(["Иван", "Дмитрий", "Степан", "Михаил"]);
+
+/*Third*/
+let changeClass = (classname, styleString) => ( Array.from(document.styleSheets)
+    .filter(sheet => !sheet.href)
+    .map(
+      sheet => Array.from(sheet.cssRules)
+        .filter(rule => rule.selectorText === `.{classname}`)
+    )
+    .filter(item => item.length > 0)
+    .map(item => item[0].cssText.split("}")
+      .join(`${styleString}}`)
+    )
+).length > 0 ? console.log("found") :
+  document.head.appendChild(
+    document.createElement("style")
+  ).textContent = `.${classname} {${styleString}}`;
+
+changeClass("second-level-menu", "background-color: red!important;");
