e0872d0b4d35f8668ff349d2a3998a31fe3adcbf
Bogdan Pavlenko <pavlenkobogdan223@gmail.com>
2019-04-29 14:23:39 +0300

    changes

--- lesson10/tenth.js
+++ lesson10/tenth.js
@@ -1,27 +1,39 @@
 let messages = [
-  "backspace",
-  "enter",
-  "shift",
-  "control",
-  "delete",
-  "space",
-  "subtract"
+    "backspace",
+    "enter",
+    "shift",
+    "control",
+    "delete",
+    "space",
+    "subtract"
 ];
 
 var log = {};
 var sendMessage = (message, callback) =>
-  setTimeout(
-    () => callback(message),
-    Math.random() * 7000
-  );
-handler = () => {
-  console.log(messages[1])
+    setTimeout(
+        () => callback(message),
+        Math.random() * 7000
+    );
+handler = message => {
+    Object.assign(log,
+        {[getKey()]: message}
+    )
 };
 messages.forEach(
-  message => sendMessage(message, handler)
+    message => sendMessage(message, handler)
 );
 
 getKey = () => {
-  var key = new Date().toLocaleString().split(", ")[1];
-  return log [key] ? key + "[2]" : key
+    var key = new Date().toLocaleString().split(", ")[1];
+    return log [key] ? key + "[2]" : key
 };
+
+//second
+function User(name) {
+    this.name = name;
+    var presence = false;
+    name !== null && presence == true ? console.log('Ivan is present') : console.log('Ivan is absent')
+}
+
+let user = new User("Ivan");
+console.info(user.presence);
