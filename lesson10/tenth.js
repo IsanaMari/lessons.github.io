9f7c5c47efbdea55625c48856a1e599e69ab196c
IsanaMari <jannapavl@gmail.com>
2019-05-01 16:32:37 +0300

    changes

--- lesson10/tenth.js
+++ lesson10/tenth.js
@@ -1,39 +1,48 @@
 let messages = [
-    "backspace",
-    "enter",
-    "shift",
-    "control",
-    "delete",
-    "space",
-    "subtract"
+  "backspace",
+  "enter",
+  "shift",
+  "control",
+  "delete",
+  "space",
+  "subtract"
 ];
 
 var log = {};
 var sendMessage = (message, callback) =>
-    setTimeout(
-        () => callback(message),
-        Math.random() * 7000
-    );
+  setTimeout(
+    () => callback(message),
+    Math.random() * 7000
+  );
 handler = message => {
-    Object.assign(log,
-        {[getKey()]: message}
-    )
+  Object.assign(log,
+    {[getKey()]: message}
+  )
 };
 messages.forEach(
-    message => sendMessage(message, handler)
+  message => sendMessage(message, handler)
 );
 
 getKey = () => {
-    var key = new Date().toLocaleString().split(", ")[1];
-    return log [key] ? key + "[2]" : key
+  var key = new Date().toLocaleString().split(", ")[1];
+  return log [key] ? key + "[2]" : key
 };
 
 //second
 function User(name) {
-    this.name = name;
-    var presence = false;
-    name !== null && presence == true ? console.log('Ivan is present') : console.log('Ivan is absent')
+  this.name = name;
+  var presence = false;
+  Object.defineProperty(this, "presence", {
+    get: () =>
+      presence ? `${this.name} is present` : `${this.name} is absent`,
+    set: value =>
+      presence = value
+  })
 }
 
 let user = new User("Ivan");
 console.info(user.presence);
+user.presence = '+';
+console.info(user.presence);
+
+// third
